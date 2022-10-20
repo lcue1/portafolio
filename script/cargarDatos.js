c89a6af37bac66576b18e4ab1285f80f05e45b8a
lcue1 <lubilluse@gmail.com>
2022-10-19 19:37:00 -0500

    archivo cargar datos agregue punto a ruta

--- script/cargarDatos.js
+++ script/cargarDatos.js
@@ -11,7 +11,7 @@ export const cargarSkillsHabilidades =  async () => {
 
 
     const imagenesHabilidades=await cargarJSON(
-        "/datos/habilidades.json",
+        "./datos/habilidades.json",
         { headers: { 'Accept': 'application/json' } },
         
     )
